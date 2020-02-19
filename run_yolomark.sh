bbb6d436c5785145d44e831672c46ff0550e40fc
Periklis S. Petridis <petrides@aristurtle.gr>
2020-02-19 15:15:36 +0200

    completed run_yolomark.sh

--- run_yolomark.sh
+++ run_yolomark.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# cd darknet/
-# ./darknet detector test data/FSCones-yolomark.data custom_cfgs/yolov3-tiny-FSCones.cfg custom-weights/yolov3-tiny-FSCones_30000.weights -dont_show -save_labels < data/yolomark-test.txt
+cd darknet/
+./darknet detector test data/FSCones-yolomark.data custom_cfgs/yolov3-tiny-FSCones.cfg custom-weights/yolov3-tiny-FSCones_30000.weights -dont_show -save_labels < data/yolomark-test.txt
 
 # Check with yolo_mark:
 
-cd ..
-git clone https://github.com/AlexeyAB/Yolo_mark.git
-cd Yolo_mark
+cd ../Yolo_mark
 cmake .
 make
 ./yolo_mark ../darknet/data/yolomark-test/ x64/Release/data/train.txt ../darknet/data/FSCones.names
